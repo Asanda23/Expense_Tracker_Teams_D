22260e05e81a1416923141474b1c02d0cf3d0f11
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-07 09:41:19 +0200

    added text js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const balance = document.getElementById("balance");
 const moneyPlus = document.getElementById("money-plus");
 const moneyMinus = document.getElementById("money-minus");
 const form = document.getElementById("form");
+const text = document.getElementById("text");
+
+
