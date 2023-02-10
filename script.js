805b1e336153592207c880a287b492ad0780932d
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-10 08:38:31 +0200

    deleted const js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-const balance = document.getElementById("balance");
-const moneyPlus = document.getElementById("money-plus");
-const moneyMinus = document.getElementById("money-minus");
-const form = document.getElementById("form");
-const text = document.getElementById("text");
+
 
 
