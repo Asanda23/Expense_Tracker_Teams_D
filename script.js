e7d0b289543490ca547414507ddd5ed8866b98cc
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-10 13:02:10 +0200

    const transactuins added

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
 
+const dummyTransactions = [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ];
