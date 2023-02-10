88f91ac4167fdee26f5e43186134cf01db9da50e
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-10 12:57:11 +0200

    const list in js added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const balance=document.getElementById('balance');
 const money_plus=document.getElementById('money_plus');
 const money_minus=document.getElementById('money_minus');
-
+const list = document.getElementById("list");
 
 
 
