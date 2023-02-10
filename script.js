c1955f060127d516a21ac423353b8920768ad06f
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-10 12:48:36 +0200

    const money minus in js added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 //unchanging const and keep varible 
 const balance=document.getElementById('balance');
-const money_plus=document.getElementById('money_plus')
+const money_plus=document.getElementById('money_plus');
+const money_minus=document.getElementById('money_minus');
+
+
 
 
