7279fa3fa8c3f3b861d8fb364ab626efcbe76366
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-13 15:37:04 +0200

    const income = amounts JS added

--- script.js
+++ script.js
@@ -65,3 +65,7 @@ function updateValues()
   const amounts = transactions.map(transaction => transaction.amount);
 
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+        
+  
