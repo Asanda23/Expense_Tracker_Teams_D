b148120ae2ff361762075fc24672e6b815d8e9a2
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-13 15:42:10 +0200

    balance.innerText JS added

--- script.js
+++ script.js
@@ -74,5 +74,7 @@ function updateValues()
         const expense = (
           amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
           -1
-      ).toFixed(2);
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
   
