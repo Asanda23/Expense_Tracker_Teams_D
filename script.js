896a6c2a188ac9b63698a36251e008077e705084
Asanda23 <asandaxhwantini68@gmail.com>
2023-02-14 08:44:49 +0200

    transaction in js added

--- script.js
+++ script.js
@@ -10,3 +10,6 @@ const amount = document.getElementById('amount');
 const localStorageTransactions = JSON.parse(
     localStorage.getItem('transactions')
 );
+
+let transactions =
+    localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
